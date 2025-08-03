0ecdde9d645d67c8ff9222fa8643ab5737003b0e
nadeer786 <muhammednadeer101@gmail.com>
2025-08-03 13:22:11 +0530

    db change

--- lib/db.ts
+++ lib/db.ts
@@ -1,27 +1,31 @@
-import mongoose, { Connection } from "mongoose";
+import mongoose from "mongoose";
 
-let cachedConnection: Connection | null = null;
+const MONGODB_URI =
+  process.env.MONGODB_URI ||
+  "mongodb+srv://syslogsitsolutions:xYwF7WbgKmRAzhjc@cluster0.yky1f6e.mongodb.net/muscle-unit";
+
+if (!MONGODB_URI) {
+  throw new Error("Please define the MONGODB_URI environment variable");
+}
+
+// Global caching to prevent creating multiple connections
+let cached = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null };
+}
 
 export async function connectToDatabase() {
-  if (cachedConnection) {
-    return { connection: cachedConnection };
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(MONGODB_URI!, {
+        bufferCommands: false,
+      })
+      .then((mongoose) => mongoose);
   }
 
-  const MONGODB_URI =
-    process.env.MONGODB_URI ||
-    "mongodb+srv://syslogsitsolutions:xYwF7WbgKmRAzhjc@cluster0.yky1f6e.mongodb.net/muscle-unit";
-
-  if (!MONGODB_URI) {
-    throw new Error("Please define the MONGODB_URI environment variable");
-  }
-
-  try {
-    const connection = await mongoose.connect(MONGODB_URI);
-    cachedConnection = connection.connection;
-
-    return { connection: cachedConnection };
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-    throw new Error("Unable to connect to database");
-  }
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
