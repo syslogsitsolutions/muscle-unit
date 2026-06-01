6fe6d9962012e0dd6c99e1ea00433221129743fb
muhammed-bylukz <muhammed.bylukz@gmail.com>
2026-06-01 12:30:05 +0530

    chore: update default MongoDB connection string to production cluster URI

--- lib/db.ts
+++ lib/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const MONGODB_URI =
   process.env.MONGODB_URI ||
-  "mongodb://localhost:27017/muscle-unit";
+  "mongodb+srv://syslogsitsolutions:xYwF7WbgKmRAzhjc@cluster0.yky1f6e.mongodb.net/muscle-unit";
 
 if (!MONGODB_URI) {
   throw new Error("Please define the MONGODB_URI environment variable");
